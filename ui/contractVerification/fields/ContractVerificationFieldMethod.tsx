6862423166b045857c336eb59aab231705518582
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-07-08 22:06:23 +0530

    contract

--- ui/contractVerification/fields/ContractVerificationFieldMethod.tsx
+++ ui/contractVerification/fields/ContractVerificationFieldMethod.tsx
@@ -100,7 +100,7 @@ const ContractVerificationFieldMethod = ({ control, isDisabled, methods }: Props
     <>
       <Box mt={{ base: 10, lg: 6 }} gridColumn={{ lg: '1 / 3' }}>
         <chakra.span fontWeight={ 500 } fontSize="lg" fontFamily="heading">
-          Currently, Blockscout supports { methods.length } contract verification methods
+          Currently, Deelance supports { methods.length } contract verification methods
         </chakra.span>
         <Popover trigger="hover" isLazy placement={ isMobile ? 'bottom-end' : 'right-start' } offset={ [ -8, 8 ] }>
           <PopoverTrigger>
@@ -113,7 +113,7 @@ const ContractVerificationFieldMethod = ({ control, isDisabled, methods }: Props
               <PopoverArrow bgColor={ tooltipBg }/>
               <PopoverBody color="white">
                 <DarkMode>
-                  <span>Currently, Blockscout supports { methods.length } methods:</span>
+                  <span>Currently, Deelance supports { methods.length } methods:</span>
                   <OrderedList>
                     { methods.map(renderPopoverListItem) }
                   </OrderedList>
